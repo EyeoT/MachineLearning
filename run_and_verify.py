4aa394b073554d9cb72677cb5a08ded2b09f2cc9
Jenna Czeck <jennaczeck@gmail.com>
2016-12-08 17:32:34 -0500

    Adding plotting of gaze point onto image (should be removed later

--- run_and_verify.py
+++ run_and_verify.py
@@ -27,6 +27,10 @@ if __name__ == '__main__':
     num_accurate = 0
     for frame_set in frame_sets:
         img = cv2.imread(os.path.join(folder_path, frame_set['frame']))
+        gaze_center_x, gaze_center_y = frame_set['gaze_data']
+        gaze_center_x = int(img.shape[1] * gaze_center_x)
+        gaze_center_y = int(img.shape[0] * (1-gaze_center_y))
+        cv2.circle(img, (gaze_center_x, gaze_center_y), 2, (255, 0, 255), -1)
         color = get_lightbox_color.get_box_color(img, frame_set['gaze_data'])
         if color == frame_set['true_color']:
             num_accurate += 1
